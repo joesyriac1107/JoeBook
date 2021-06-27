e8532d644f210c63c865dd914a21cf4dad9387fb
josyriac <josyriac@microsoft.com>
2021-06-27 16:31:47 +0530

    Margin for right bar title

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -24,6 +24,62 @@ export default function RightBar() {
                         </div>
                         <span className="rightbarFriendName">John Carter</span>
                     </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <ProfileImg imgSrc = "assets/person/3.jpeg" className="rightbarProfileImg"/>
+                            <div className="rightbarOnline"></div>
+                        </div>
+                        <span className="rightbarFriendName">John Carter</span>
+                    </li>
                 </ul>
             </div>
         </div>
