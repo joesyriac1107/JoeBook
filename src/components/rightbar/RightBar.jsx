1da88bb3081760a45ffee893f0c27d3c8df0bc3a
Joe Syriac <joesyriac1107@gmail.com>
2021-06-27 08:00:25 +0530

    Right bar changes

--- src/components/rightbar/RightBar.jsx
+++ src/components/rightbar/RightBar.jsx
@@ -3,7 +3,26 @@ import './rightbar.css'
 export default function RightBar() {
     return (
         <div className='rightbar'>
-            RightBar
+            <div className="rightbarWrapper">
+                <div className="birthdayContainer">
+                    <img src="assets/gift.png" alt="" className="birthdayImg"/>
+                    <span className="birthdayText">
+                        <b>Pola Faster</b>
+                        and
+                        <b>3 other friends</b>
+                        have birthday today.
+                    </span>
+                </div>
+                <img src="assets/ad.png" alt="" className="rightbarAd"/>
+                <h4 className="rightbarTitle">Online Friends</h4>
+                <ul className="rightbarFriendList">
+                    <li className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                            <img src="assets/person/3.jpeg" alt="" className="rightbarProfileImg"/>
+                        </div>
+                    </li>
+                </ul>
+            </div>
         </div>
     )
 }
