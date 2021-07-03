a7e1f69b6d5f94f36427098d5728a0ea7766c808
josyriac <josyriac@microsoft.com>
2021-07-03 11:33:43 +0530

    dummy data

--- src/components/post/Post.jsx
+++ src/components/post/Post.jsx
@@ -1,29 +1,32 @@
-import {MoreVert} from '@material-ui/icons'
+import { MoreVert } from '@material-ui/icons'
 import ProfileImg from '../common/profile/image/ProfileImg'
 import './post.css'
+import { Users } from '../../dummyData'
 
-export default function Post() {
+export default function Post({ post }) {
+    console.log(Users)
+    const user = Users.filter(user => user.id === post.userId)[0]
     return (
         <div className="post">
             <div className="postWrapper">
                 <div className="postTop">
                     <div className="postTopLeft">
-                        <ProfileImg className='postProfileImg' imgSrc='/assets/person/1.jpeg'/>
-                        <span className="postUserName">Joe</span>
+                        <ProfileImg className='postProfileImg' imgSrc={user.profilePicture} />
+                        <span className="postUserName">{user.username}</span>
                         <span className="postDate">5 mins ago</span>
                     </div>
                     <div className="postTopRight">
-                        <MoreVert/>
+                        <MoreVert />
                     </div>
                 </div>
                 <div className="postCenter"></div>
-                    <span className="postText">Hey! Its my first post</span>
-                    <img src="/assets/post/1.jpeg" alt="" className="postImg" />
+                <span className="postText">{post.desc}</span>
+                <img src={post.photo} alt="" className="postImg" />
                 <div className="postBottom">
                     <div className="postBottomLeft">
-                        <img className ='likeIcon'src="/assets/like.png" alt="" />
-                        <img className ='likeIcon'src="/assets/heart.png" alt="" />
-                        <span className="postLikeCounter">32 people liked it</span>
+                        <img className='likeIcon' src="/assets/like.png" alt="" />
+                        <img className='likeIcon' src="/assets/heart.png" alt="" />
+                        <span className="postLikeCounter">{post.like} people liked it</span>
                     </div>
                     <div className="postBottomRight">
                         <span className="postCommentText">
